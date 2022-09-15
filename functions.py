6734cdc1ba65082b96c3da8182d057fe9ed136e6
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2022-09-15 17:27:22 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,289 @@
+import cv2
+import mss
+import numpy
+import pyautogui
+from time import sleep
+import keyboard
+from datetime import datetime
+from hsvfilter import HsvFilter
+import win32gui
+import win32con
+import os
+def clear(): return os.system('cls')
+
+
+sct = mss.mss()
+
+cork = cv2.imread('img/cork.jpg', cv2.IMREAD_UNCHANGED)
+processed_cork = cv2.imread('img/processedCork.jpg', cv2.IMREAD_UNCHANGED)
+filter1 = HsvFilter(111, 0, 59, 179, 254, 255, 103, 0, 255, 54)
+processed_cork2 = cv2.imread(
+    'img/processedCork.jpg', cv2.IMREAD_UNCHANGED)  # cv2.IMREAD_UNCHANGED
+filter2 = HsvFilter(115, 0, 0, 179, 255, 203, 226, 0, 115, 36)
+rod = cv2.imread('img/rod.jpg', cv2.IMREAD_GRAYSCALE)
+# cv2.COLOR_BGR2GRAY
+rodLeft = cv2.imread('img/rodLeft.jpg', cv2.IMREAD_GRAYSCALE)
+fishing = cv2.imread('img/fishing.jpg', cv2.IMREAD_GRAYSCALE)
+pull = cv2.imread('img/pullFish.jpg', cv2.IMREAD_UNCHANGED)
+fishCaught = cv2.imread('img/fishCaught.jpg', cv2.IMREAD_UNCHANGED)
+
+fishing_dimensions = {
+    'left': 750,
+    'top': 820,
+    'width': 420,
+    'height': 145
+}
+
+player_dimensions = {
+    'left': 860,
+    'top': 450,
+    'width': 200,
+    'height': 150
+}
+# dimensions = {
+#     'left': 880,
+#     'top': 560,
+#     'width': 160,
+#     'height': 20
+# }
+dimensions_left = {
+    'left': 882,
+    'top': 565,
+    'width': 40,
+    'height': 20
+}
+dimensions = {
+    'left': 1005,
+    'top': 565,
+    'width': 40,
+    'height': 20
+}
+full_dimensions = {
+    'left': 0,
+    'top': 0,
+    'width': 1920,
+    'height': 1080
+}
+
+
+def checkCork(sct, img=processed_cork, filter=filter1, Type=1, dimensions_right=dimensions, dimensions_left=dimensions_left):
+    if checkRod(sct)[2]:
+        dimensions = dimensions_right
+    else:
+        dimensions = dimensions_left
+    scr = numpy.array(sct.grab(dimensions))
+    scr = apply_hsv_filter(scr, filter, Type)
+    result = cv2.matchTemplate(
+        scr, img, cv2.TM_CCOEFF_NORMED)  # cv2.TM_CCOEFF_NORMED
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    return max_val, result
+
+
+def checkRod(sct, img=rod, imgl=rodLeft, dimensions=player_dimensions):
+    scr = numpy.array(sct.grab(dimensions))
+    scr_remove = cv2.cvtColor(scr, cv2.COLOR_BGR2GRAY)  # scr[:, :, :3]
+    result = cv2.matchTemplate(
+        scr_remove, img, cv2.TM_CCOEFF_NORMED)  # cv2.TM_CCOEFF_NORMED
+    resultL = cv2.matchTemplate(
+        scr_remove, imgl, cv2.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    _, max_valL, _, max_locL = cv2.minMaxLoc(resultL)
+    if max_valL > max_val + (max_val * 20 / 100):
+        rodRight = False
+        return max_valL, resultL, rodRight
+    else:
+        rodRight = True
+        return max_val, result, rodRight
+
+
+def throwRod():
+    pyautogui.press('5')
+    sleep(0.1)
+    pyautogui.hotkey('alt', '1')
+    return
+
+
+def Fishing(sct, img=fishing, dimensions=fishing_dimensions):
+    scr = numpy.array(sct.grab(dimensions))
+    scr_remove = cv2.cvtColor(scr, cv2.COLOR_BGR2GRAY)  # scr[:, :, :3]
+    result = cv2.matchTemplate(
+        scr_remove, img, cv2.TM_CCOEFF_NORMED)  # cv2.TM_CCOEFF_NORMED
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    return max_val
+
+
+def pullCheck(sct, img=pull, dimensions=fishing_dimensions):
+    scr = numpy.array(sct.grab(dimensions))
+    scr_remove = scr[:, :, :3]
+    result = cv2.matchTemplate(
+        scr_remove, img, cv2.TM_CCOEFF_NORMED)  # cv2.TM_CCOEFF_NORMED
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    return max_val, result
+
+
+def pullRod():
+    pyautogui.keyDown('alt')
+    pyautogui.keyDown('1')
+
+
+def releaseRod():
+    pyautogui.keyUp('1')
+    pyautogui.keyUp('alt')
+
+
+def caughtCheck(fishes, caught, sct, img=fishCaught, dimensions=fishing_dimensions):
+    scr = numpy.array(sct.grab(dimensions))
+    scr_remove = scr[:, :, :3]
+    result = cv2.matchTemplate(
+        scr_remove, img, cv2.TM_CCOEFF_NORMED)  # cv2.TM_CCOEFF_NORMED
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    if max_val >= .82:
+        fishes += 1
+        caught = True
+        releaseRod()
+        return fishes, caught, result
+    else:
+        return fishes, caught, result
+
+
+def apply_hsv_filter(sct, hsv_filter, Type):
+    if Type == 1:
+        name = 'filter1'
+    else:
+        name = 'filter2'
+    hsv = cv2.cvtColor(sct, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    s = shift_channel(s, hsv_filter.sAdd)
+    s = shift_channel(s, -hsv_filter.sSub)
+    v = shift_channel(v, hsv_filter.vAdd)
+    v = shift_channel(v, -hsv_filter.vSub)
+    hsv = cv2.merge([h, s, v])
+    lower = numpy.array([hsv_filter.hMin, hsv_filter.sMin, hsv_filter.vMin])
+    upper = numpy.array([hsv_filter.hMax, hsv_filter.sMax, hsv_filter.vMax])
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(hsv, hsv, mask=mask)
+    img = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
+    # scale_percent = 500  # percent of original size
+    # width = int(img.shape[1] * scale_percent / 100)
+    # height = int(img.shape[0] * scale_percent / 100)
+    # dim = (width, height)
+    # img_resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    # cv2.imshow(name, img_resized)
+    # cv2.waitKey(1)
+    return img
+
+
+def shift_channel(c, amount):
+    if amount > 0:
+        lim = 255 - amount
+        c[c >= lim] = 255
+        c[c < lim] += amount
+    elif amount < 0:
+        amount = -amount
+        lim = amount
+        c[c <= lim] = 0
+        c[c > lim] -= amount
+    return c
+
+
+def showRectangles(sct, result, threshold, target, dimensions, text, fullScreen=full_dimensions):
+    try:
+        scr = numpy.array(sct.grab(fullScreen))
+    except:
+        scr = sct
+    w = target.shape[1]
+    h = target.shape[0]
+    rectangles = []
+    try:
+        yloc, xloc = numpy.where(result[1] >= threshold)
+    except:
+        yloc, xloc = numpy.where(result[2] >= threshold)
+    for (x, y) in zip(xloc, yloc):
+        rectangles.append([int(x + dimensions['left']),
+                          int(y + dimensions['top']), int(w), int(h)])
+        rectangles.append([int(x + dimensions['left']),
+                           int(y + dimensions['top']), int(w), int(h)])
+    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.3)
+    for (x, y, w, h) in rectangles:
+        cv2.rectangle(scr, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        org = (int(x), int(y + h + 15))
+        fontFace = cv2.FONT_HERSHEY_DUPLEX
+        fontScale = 0.5
+        color = (0, 255, 25)
+        scr = cv2.putText(scr, text, org, fontFace, fontScale, color)
+    return scr
+
+
+def showDetectionArea(sct, dimensions, text, fullScreen=full_dimensions):
+    try:
+        scr = numpy.array(sct.grab(fullScreen))
+    except:
+        scr = sct
+    cv2.rectangle(scr, (dimensions['left'], dimensions['top']), (
+        dimensions['left'] + dimensions['width'], dimensions['top'] + dimensions['height']), (0, 0, 255), 2)
+    org = (dimensions['left'], dimensions['top'] - 10)
+    fontFace = cv2.FONT_HERSHEY_DUPLEX
+    fontScale = 0.5
+    color = (0, 0, 255)
+    scr = cv2.putText(scr, text, org, fontFace, fontScale, color)
+    return scr
+
+
+def showFrames(sct, start, duration, Type, dimensions_right=dimensions, dimensions_left=dimensions_left):
+
+    while True:
+        if checkRod(sct)[2]:
+            dimensions = dimensions_right
+        else:
+            dimensions = dimensions_left
+        if Type == 1:
+            img = showRectangles(sct, checkCork(
+                sct), 0.55, processed_cork, dimensions, 'cork  **1**')
+            name = 'fishing with filter1'
+        else:
+            img = showRectangles(sct, checkCork(
+                sct, processed_cork2, filter2, 2), 0.55, processed_cork2, dimensions, 'cork  **2**')
+            name = 'fishing with filter2'
+        img = showRectangles(img, checkRod(sct), 0.70,
+                             rod, player_dimensions, 'Rod')
+        img = showRectangles(img, pullCheck(sct), 0.80, pull,
+                             fishing_dimensions, 'Fishing')
+        img = showRectangles(img, caughtCheck(0, None, sct),
+                             0.82, fishCaught, fishing_dimensions, 'Caught!')
+        img = showDetectionArea(showDetectionArea(showDetectionArea(
+            img, fishing_dimensions, 'Fishing area'), player_dimensions, 'Player area'), dimensions, 'Cork area')
+        cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(name, 440, 500)
+        hWnd = win32gui.FindWindow(None, name)
+        win32gui.SetWindowPos(hWnd, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+                              win32con.SWP_SHOWWINDOW | win32con.SWP_NOSIZE | win32con.SWP_NOMOVE)
+        cv2.imshow(name, img[400:900, 740:1180])
+        cv2.waitKey(1)
+        if keyboard.is_pressed('q') or divmod((datetime.now() - start).total_seconds(), 60)[0] >= duration:
+            break
+
+
+def quit(sct, start, duration, trys, items, fishes, fails, minute, Type):
+    if keyboard.is_pressed('q') or divmod((datetime.now() - start).total_seconds(), 60)[0] >= duration:
+        end = datetime.now()
+        clear()
+        if checkRod(sct)[0] >= .60:
+            throwRod()
+        total_duration = int(divmod((end - start).total_seconds(), 60)[0])
+        clear()
+        print(
+            f'\nSession started at: {start.strftime("%H:%M:%S")}')
+        print(
+            f'Trys: {trys}\nItems: {items}\nFishes: {fishes}\nFails: {fails}')
+        print(
+            f'Session ended at: {end.strftime("%H:%M:%S")}')
+        print(
+            f'Session duration: {total_duration} {minute}')
+        try:
+            print(
+                f'Catches per minute ratio: {(items + fishes) / total_duration}')
+        except:
+            print(
+                'Too little time to be able to calculate the catches/minute ratio')
+        print(f'Filter type: {Type}')
+        return True
---
+++ main.py
@@ -0,0 +1,183 @@
+import mss
+from time import time, sleep
+import pyautogui
+import keyboard
+from functions import *
+from datetime import datetime
+from threading import Thread
+import os
+def clear(): return os.system('cls')
+
+
+def main():
+    sct = mss.mss()
+    Type = 1
+    cork = processed_cork
+    filter = filter1
+    cork_th = 0.60
+
+    duration = 60
+    debug = False
+    trys, items, fishes, fails, minute = 0, 0, 0, 0, 'minute'
+    choice = input(
+        '\nDebug mode disabled by default\nDo you want to enable it? Y/N ')
+    if choice.lower() == 'y':
+        debug = True
+        print('\nDebug mode enabled')
+    else:
+        print('\nDebug mode disabled')
+    try:
+        duration_choice = int(input(
+            f'\nDefault session duration is {duration} minutes\ninsert another duration in minutes if you want to change it: '))
+        if duration_choice > 0 and duration_choice != duration:
+            duration = duration_choice
+        if duration != 1:
+            minute += 's'
+        print(f'Assigned new duration for the session [{duration} {minute}]')
+    except:
+        minute = 'minutes'
+        print(f'Session duration "default" {duration} {minute}')
+        pass
+
+    print(f'\nDefault detection mode is "1"\nYou can try with detection mode "2"')
+    print("""
+Data on the effectiveness of the modes in the waters of "The sunken Sea"
+Tests duration: 60 minutes
+mode 1:
+    try's       259
+    caught's    257
+    accuracy    99,22%
+    ratio       4,28 c/min
+mode 2:
+    try's       285
+    caught's    273
+    accuracy    98,24%
+    ratio       4.55 c/min
+    """)
+    while True:
+        try:
+            detection_choice = int(input('Chose detection mode [1 or 2]: '))
+            if detection_choice != 2 and detection_choice != 1:
+                print('You only have 2 options [1 or 2] leave the field blank')
+            elif detection_choice == 2:
+                Type = 2
+                cork = processed_cork2
+                filter = filter2
+                cork_th = 0.58
+                print(f'\nSelected filter type: {Type}')
+                break
+            else:
+                break
+        except:
+            print(f'\nSelected filter type: {Type}')
+            break
+
+    def fishingBot(sct, start, duration):
+        print("\nPress 'q' to quit.")
+        print('\nStarting bot!\n')
+        sleep(2)
+        trys = 0
+        items = 0
+        fishes = 0
+        fails = 0
+        coords = 1440, 540
+        tiks = 0
+        pyautogui.click(coords)
+        sleep(0.2)
+        pyautogui.keyDown('d')
+        sleep(0.1)
+        pyautogui.keyUp('d')
+        sleep(1)
+        throwRod()
+        sleep(1)
+        if checkRod(sct)[0] < .45:
+            coords = 480, 540
+            pyautogui.keyDown('a')
+            sleep(0.5)
+            pyautogui.keyUp('a')
+            sleep(0.2)
+            pyautogui.click(coords)
+            sleep(0.2)
+            throwRod()
+            sleep(1)
+            if checkRod(sct)[0] < .40:
+                print(
+                    'Rod not detected, try to place your character closer to the water')
+                exit()
+        sleep(2)
+        trys += 1
+        Time = time()
+        while True:
+            if (time() - Time) > 10:
+                if checkRod(sct)[0] < .65:
+                    pyautogui.click(coords)
+                    sleep(0.1)
+                    throwRod()
+                    sleep(2)
+                    fails += 1
+                    trys += 1
+                    Time = time()
+                Time = time()
+            if checkCork(sct, cork, filter, Type)[0] >= cork_th:
+                tiks += 1
+            if tiks >= 3:
+                throwRod()
+                tiks = 0
+                if Fishing(sct) >= .50:
+                    caught = False
+                    Time = time()
+                    while Fishing(sct) > .50:
+                        Time = time()
+                        if pullCheck(sct)[0] >= .70:
+                            pullRod()
+                        else:
+                            releaseRod()
+                        fishes, caught, _ = caughtCheck(fishes, caught, sct)
+                        if caught:
+                            sleep(0.8)
+                            throwRod()
+                            sleep(0.2)
+                        if quit(sct, start, duration, trys, items, fishes, fails, minute, Type):
+                            break
+                    if not caught:
+                        fails += 1
+                        releaseRod()
+                        sleep(0.1)
+                        throwRod()
+                    caught = False
+                    Time = time()
+                else:
+                    items += 1
+                    Time = time()
+                    sleep(0.5)
+                    throwRod()
+                clear()
+                print(f'\nSession started at: {start.strftime("%H:%M:%S")}')
+                end = datetime.now()
+                print(
+                    f'Trys: {trys}\nItems: {items}\nFishes: {fishes}\nFails: {fails}')
+                print(
+                    f'Actual session duration: {int(divmod((end - start).total_seconds(), 60)[0])} {minute}')
+                trys += 1
+                sleep(3)
+                if checkRod(sct)[0] < .65:
+                    fails += 1
+                    trys += 1
+                    pyautogui.click(coords)
+                    sleep(0.1)
+                    throwRod()
+                    sleep(2)
+                Time = time()
+            if quit(sct, start, duration, trys, items, fishes, fails, minute, Type):
+                break
+        return
+    start = datetime.now()
+    t1 = Thread(target=fishingBot, args=(sct, start, duration,))
+    t1.start()
+    if debug:
+        t2 = Thread(target=showFrames, args=(sct, start, duration, Type,))
+        t2.start()
+
+
+if __name__ == '__main__':
+    main()
